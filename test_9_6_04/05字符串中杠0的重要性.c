0df151a427a8e4b22aa1c353cbed86ddc05da96c
养鱼捉 <2678660795@qq.com>
2021-09-06 22:12:41 +0800

    第四个解决方案

---
+++ test_9_6_04/05字符串中杠0的重要性.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+//下面代码，打印结果是什么？为什么？（突出'\0'的重要性）
+int main()
+{
+	char arr1[] = "bit";
+	char arr2[] = { 'b', 'i', 't' };
+	char arr3[] = { 'b', 'i', 't', '\0' };
+	printf("%s\n", arr1);
+	printf("%s\n", arr2);
+	printf("%s\n", arr3);
+	return 0;
+}
